8613529790c575865a19f1d96ea0514de7610456
MauroArys <mauro.arys@student.odisee.be>
2026-09-26 14:31:47 +0200

    kleine update imports

--- Unit1.3/TaskManager/app/src/main/java/be/odisee/MauroArystaskmanager/MainActivity.kt
+++ Unit1.3/TaskManager/app/src/main/java/be/odisee/MauroArystaskmanager/MainActivity.kt
@@ -9,7 +9,6 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -27,9 +26,9 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             TaskManagerTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) {
+
                     AllTasksCompletedScreen()
-                }
+
             }
         }
     }
